2f153122aebd30c1d0aad52661727642dcf4ec07
Kyle Teague <kyteague@gmail.com>
2026-09-03 22:44:44 -0300

    fix per-post open graph metadata

--- src/consts.ts
+++ src/consts.ts
@@ -3,5 +3,6 @@
 
 export const SITE_TITLE = "Kyle Teague";
 export const SITE_DESCRIPTION =
-	"Software engineer and fractional CTO helping teams build durable systems and make clear technology decisions.";
+  "Software engineer and fractional CTO helping teams build durable systems and make clear technology decisions.";
 export const SITE_AUTHOR = "Kyle Teague";
+export const SITE_TWITTER = "@kyteague";
